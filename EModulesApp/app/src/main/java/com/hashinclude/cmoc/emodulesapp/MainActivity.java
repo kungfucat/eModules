ab934ee799459621be6b1edb7b625b912b363989
Harsh Bhardwaj <harshbhardwaj2357@gmail.com>
2018-02-07 10:06:57 +0530

    Opened and read data from db and added QuestionModel

--- EModulesApp/app/src/main/java/com/hashinclude/cmoc/emodulesapp/MainActivity.java
+++ EModulesApp/app/src/main/java/com/hashinclude/cmoc/emodulesapp/MainActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     //main RecyclerView will hold the data to show on the MainScreen
     RecyclerView mainRecyclerView;
+    DatabaseAdapter databaseAdapter;
+    ArrayList<QuestionModel> questionModelArrayList;
     Context context;
 
     @Override
@@ -23,13 +28,18 @@ public class MainActivity extends AppCompatActivity {
         mainRecyclerView = findViewById(R.id.mainRecyclerView);
         context = this;
 
+        databaseAdapter = new DatabaseAdapter(this);
+        questionModelArrayList = databaseAdapter.getAllData();
+        for (int i = 0; i < questionModelArrayList.size(); i++) {
+            QuestionModel model = questionModelArrayList.get(i);
+            Log.d("MODEL", model.getId() + " : " + model.getTimeTaken());
+        }
 
         MainRecyclerViewAdapter adapter = new MainRecyclerViewAdapter(this);
         mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mainRecyclerView.setAdapter(adapter);
         mainRecyclerView.addItemDecoration(new DividerItemDecoration(this,
                 DividerItemDecoration.VERTICAL));
-
 
         //Added the OnTouchListener
         mainRecyclerView.addOnItemTouchListener(new RowClickedListener(this, mainRecyclerView, new OnItemClickListener() {
